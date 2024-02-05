e549a097e40533eb13a1529f236f117f46b5d32b
Myles Hill <mylehill99@gmail.com>
2024-02-05 12:12:40 -0500

    finished

--- app.js
+++ app.js
@@ -3,8 +3,26 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+const firstName = 'Myles';
+let lastName; 
+var age; 
 
-// Exercise 2
+lastName = 'Hill';
+age = 23;
 
-// Exercise 3
+let language = 'JavaScript';
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = 'M';
+
+let title = 'Name of the Wind';
+let author = 'Patrick Rothfuss';
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
